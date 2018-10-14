bcb769168b035bc4d845cc56fe331a929963a938
3A532083 <luckyday604@gm.student.ncutedu.tw>
2018-10-14 17:58:16 +0800

    someone

---
+++ src/Demo/Hello/Someone.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 2018/10/14
+ * Time: 下午 05:57
+ */
+namespace Demo\Hello;
+
+class Someone
+{
+    function __construct($name)
+    {
+        echo 'Hello '.$name.PHP_EOL;
+    }
+}
