7506a8f2691fd833ed42bcf37eedb052366da76a
3A532083 <luckyday604@gm.student.ncutedu.tw>
2018-10-14 17:56:32 +0800

    no message

---
+++ src/Demo/Hello/Lara.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 2018/10/14
+ * Time: 下午 05:55
+ */
+namespace Demo\Hello;
+
+class Lara
+{
+    function __construct()
+    {
+        echo 'Hello Lara~'.PHP_EOL;
+    }
+}
---
+++ src/Demo/HelloWorld.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 2018/10/14
+ * Time: 下午 05:49
+ */
+
+      namespace Demo;
+
+      class HelloWorld
+      {
+          function __construct()
+          {
+              echo 'Hello World ~'.PHP_EOL;
+          }
+      }
